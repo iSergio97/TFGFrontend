749e6dad440d18960c18db35a8998e76ad844998
Sergio <sergiotb15@gmail.com>
2020-11-18 19:30:21 +0100

    [E] - App.vue
    Modificación App.vue para la prueba de conexión con el backend

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="nav">
+    <h1> Hola!</h1>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  name: 'App',
+};
+/*
+// eslint-disable-next-line no-console
+console.log('Carga script de App.vue');
+axios.post('http://localhost:8080/operacion/administrador/new', {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  params: {
+    cookieSession: 'Sergio',
+    tipo: 'A',
+  },
+// eslint-disable-next-line no-console
+}).then((res) => console.log(res));
+ */
+</script>
+
+<style>
+
+</style>
