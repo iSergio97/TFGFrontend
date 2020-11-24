01e8d296440bf9f2360b07078351fa3107c246ac
Sergio <sergiotb15@gmail.com>
2020-11-24 20:14:03 +0100

    [E] - Login
    Mejoras estilos del login, añadiendo mensaje de error cuando el usuario y la contraseña no coinciden.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,12 @@
+/* eslint-disable */
 <template>
   <div class="login-body content has-text-centered">
     <h3> {{headerLang[lang]}} </h3>
     <form @submit.prevent="submitForm">
+      <div v-if="submitError" class="login-error is-rounded">
+        <i class="fa fa-warning"></i>
+        {{errorFormRes[lang]}}
+      </div>
       <div class="login-form">
         <div class="login-username">
           <div>
@@ -16,7 +21,11 @@
           </div>
         </div>
         <div class="login-button">
-          <button class="button is-info is-light"> Prueba </button>
+          <!-- submitted ? 'button is-loading' : 'button is-success is-inverted is-rounded' -->
+          <button :class="submitted ? 'button is-loading is-rounded'
+           : 'button is-success is-inverted is-rounded'">
+            {{ submitButton[lang] }}
+          </button>
         </div>
       </div>
     </form>
@@ -26,25 +35,40 @@
 <script>
 import { ref } from 'vue';
 import Cookie from 'js-cookie';
+import axios from 'axios';
 
 export default {
   name: 'Login',
   setup() {
     const session = Cookie.get('JSESSIONID');
-    const lang = (Cookie.get('lang') !== undefined && Cookie.get('lang') < 2) ? Cookie.get('lang') : 0;
     if (session) {
       window.location.href = '';
     }
+    const lang = (Cookie.get('lang') !== undefined && Cookie.get('lang') < 2) ? Cookie.get('lang') : 0;
     const headerLang = ref(['Iniciar sesión', 'Login']);
     const usernameLang = ref(['Nombre de usuario', 'Username']);
     const passwordLang = ref(['Contraseña', 'Password']);
     const errorLang = ref(['Debe rellenar todos los campos', 'Both inputs are required']);
     const username = ref('');
     const password = ref('');
+    const submitButton = ref(['Enviar', 'Submit']);
+    const submitted = ref(false);
+    const errorFormRes = ref(['El nombre de usuario o contraseña no son correctos',
+      'Username and password are not correct']);
+    const submitError = ref(false);
     const submitForm = () => {
       if (username.value.trim() === '' || password.value.trim() === '') {
         alert(errorLang.value[lang]);
+        return;
       }
+      submitted.value = true;
+      axios.get(`http://localhost:8080/habitante/login?username=${username.value}&password=${password.value}`)
+        .then((res) => console.log(res))
+        .catch((res) => {
+          console.log(res);
+          submitError.value = true;
+        });
+      submitted.value = false;
     };
     return {
       username,
@@ -53,6 +77,10 @@ export default {
       passwordLang,
       headerLang,
       lang,
+      submitted,
+      submitButton,
+      errorFormRes,
+      submitError,
       submitForm,
     };
   },
@@ -60,11 +88,32 @@ export default {
 </script>
 
 <style scoped>
+@import url('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 * {
   padding-top: 1%;
 }
 
 input {
   width: 17.5%;
+}
+
+.login-error {
+  color: #D8000C;
+  margin: auto;
+  width: 30%;
+  background-color: #FFD2D2;
+  padding-bottom: 1%;
+}
+
+.login-sucess {
+  color: #4F8A10;
+}
+
+.login-info {
+  color: #00529B;
+}
+
+.login-warning {
+  color: #D8000C;
 }
 </style>
